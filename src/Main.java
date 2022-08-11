5f1d00e36b79ce7ed68b71128f172d8c5a9c49df
Kyle Coleman <kcoleman20@cnm.edu>
2022-08-11 09:32:46 -0600

    Swap values practice, currently recursive, throws error.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
+
 public class Main {
 
   public static void main(String[] args) {
 
   String str = "Twenty four";
   System.out.println(reverse(str));
-    
+
+  System.out.println(swapValues(10, 20));
   }
   
   public static String reverse(String in) {
+    // Handles the exception if the string isn't a proper input
     if (in == null) {
       throw new IllegalArgumentException("Null is not a valid input");
     }
+    // Created a StringBuilder
     StringBuilder out = new StringBuilder();
-    
+
+    // Holds the array of characters of 'str'
     char[] chars = in.toCharArray();
-    
+
+    // Iterates over the string starting at the back
+    // ... while the index is above zero, continues to count down and print each char
     for (int i = chars.length - 1; i >= 0; i--) {
       out.append(chars[i]);
     }
     return out.toString();
   }
-  
+
+  public static int swapValues(int a, int b) {
+
+    b = b + a;
+    a = b - a;
+    b = b - a;
+
+    return swapValues(10, 20);
+  }
 
 }
